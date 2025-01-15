dabd9832f5c4dc69d53a5286b92a014b412755ed
guswns3371 <guswns3371@gmail.com>
2025-01-15 22:51:56 +0900

    programmers: 충돌위험 찾기
    이게 어떻게 레벨 2임;;

---
+++ src/Programmers/lv2/충돌위험 찾기.py
@@ -0,0 +1,47 @@
+from collections import Counter
+
+
+def find_best_route(points, route):
+    q = []
+    time = 0
+
+    for i in range(len(route) - 1):
+        sx, sy = points[route[i] - 1]
+        ex, ey = points[route[i + 1] - 1]
+        # x 먼저 조절
+        while sx != ex:
+            q.append((sx, sy, time))
+            if sx < ex:
+                sx += 1
+            else:
+                sx -= 1
+            time += 1
+
+        # y 그 다음 조절
+        while sy != ey:
+            q.append((sx, sy, time))
+            if sy < ey:
+                sy += 1
+            else:
+                sy -= 1
+            time += 1
+
+    q.append((sx, sy, time))
+    return q
+
+
+def solution(points, routes):
+    total = []
+    for route in routes:
+        total.extend(find_best_route(points, route))
+
+    answer = 0
+    for v in Counter(total).values():
+        if v > 1:
+            answer += 1
+    return answer
+
+
+solution([[3, 2], [6, 4], [4, 7], [1, 4]], [[4, 2], [1, 3], [2, 4]])
+solution([[3, 2], [6, 4], [4, 7], [1, 4]], [[4, 2], [1, 3], [4, 2], [4, 3]])
+solution([[2, 2], [2, 3], [2, 7], [6, 6], [5, 2]], [[2, 3, 4, 5], [1, 3, 4, 5]])
