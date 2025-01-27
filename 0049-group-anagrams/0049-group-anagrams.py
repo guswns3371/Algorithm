7e1ae3e3d73d039d6f7054e7b0ed1f6598021c13
hyyyynjn <guswns3371@gmail.com>
2025-01-27 23:10:38 +0900

    Time: 10 ms (87.43%), Space: 20.6 MB (77.99%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,7 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        def make_key(word):
-            key_list = [0 for _ in range(26)]
-            for i in range(len(word)):
-                key_list[ord(word[i]) % 26] += 1
-            return tuple(key_list)
-
-
         ret = defaultdict(list)
         for word in strs:
-            ret[make_key(word)].append(word)
+            key = "".join(sorted(word))
+            ret[key].append(word)
         return list(ret.values())
